242ea7050b253240285ca4ce4eddc24c75552db0
hzy <h1260071766@126.com>
2022-05-30 03:13:24 +0800

    luci: colorful status luci: Fix bug: 500 when missing config

--- luci-app-mnh/luasrc/model/cbi/mnh/status.lua
+++ luci-app-mnh/luasrc/model/cbi/mnh/status.lua
@@ -1,6 +1,8 @@
 local dsp = require "luci.dispatcher"
 local tpl = require "luci.template"
 
+local kvmsg = '%s: <font color="blue">%s</font><br />'
+
 local m = Map("mnh", translate("mnh"))
 
 local s = m:section(TypedSection, "instance", translate("Instance"))
@@ -43,9 +45,12 @@ o.rawhtml = true
 function o.cfgvalue(self, section)
 	local i, n
 	local str = ""
-	for i, n in pairs({"type", "port", "server", "service"}) do
-		str = str .. n .. ": " .. self.map:get(section, n) .. "<br />"
-	end
+
+	str = str .. kvmsg:format(translate("Type"), self.map:get(section, "type") or "")
+	str = str .. kvmsg:format(translate("port"), self.map:get(section, "port") or "0")
+	str = str .. kvmsg:format(translate("server"), self.map:get(section, "server") or "")
+	str = str .. kvmsg:format(translate("service"), self.map:get(section, "service") or "")
+
 	return str
 end
 
@@ -58,16 +63,26 @@ local tpl_script = tpl.Template(nil, [[
 						var e = document.getElementById(`cbi-mnh-${i}-status`);
 						if(e) {
 							switch(st[i].status) {
+								case "not running":
+									e.innerHTML = `<font color="red"><%=translate("not running")%></font>`;
+									break;
 								case "connecting":
-								default:
-									e.innerHTML = `${st[i].status}`;
+									e.innerHTML = `<font color="orange"><%=translate("connecting")%></font>`;
 									break;
 								case "fail":
-									e.innerHTML = `${st[i].status}<br />${st[i].error}`;
+									e.innerHTML = `<font color="red"><%=translate("fail")%></font>` + "<br />"
+													`<font color="red">${st[i].error}</font>`;
 									break;
 								case "success":
+									e.innerHTML = `<font color="green"><%=translate("success")%></font>` + "<br />" + 
+													`<font color="blue">${st[i].addr}</font>`;
+									break;
 								case "disconnected":
-									e.innerHTML = `${st[i].status}<br />${st[i].addr}`;
+									e.innerHTML = `<font color="orange"><%=translate("disconnected")%></font>` + "<br />" + 
+													`<font color="blue">${st[i].addr}</font>`;
+									break;
+								default:
+									e.innerHTML = `<font color="orange">${st[i].status}</font>`;
 									break;
 							}
 						}
